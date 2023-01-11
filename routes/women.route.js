0ace80c99bb75ee63cb407a21ca843f8d9c9eba1
ahmedraza90 <34ahmed.raza@gmail.com>
2023-01-12 02:47:38 +0500

    temp

--- routes/women.route.js
+++ routes/women.route.js
@@ -1,7 +1,7 @@
 const { router } = require("../app")
 const controller = require("../controllers/women.controller")
 
-router.post('/save-women',controller.save_women)
+router.post('/women-on-mars',controller.save_women)
 router.get('/women-get-all-users',controller.get_all_users)
 router.get('/women-get-user-by',controller.get_user_by)
 

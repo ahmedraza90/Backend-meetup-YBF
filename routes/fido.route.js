0c848ebed7b0b3018905dea3a0b116279046002b
ahmedraza90 <34ahmed.raza@gmail.com>
2024-01-26 19:35:27 +0500

    fido

--- routes/fido.route.js
+++ routes/fido.route.js
@@ -1,7 +1,7 @@
 const { router } = require("../app")
 const controller = require("../controllers/fido.controller.js")
 
-router.post('/merkleRoot',controller.merkle_root)
-router.post('/check-wallet/:wallet',controller.get_user_by)
+router.get('/merkleRoot',controller.merkle_root)
+router.get('/check-wallet/:wallet',controller.get_user_by)
 
 module.exports = router
